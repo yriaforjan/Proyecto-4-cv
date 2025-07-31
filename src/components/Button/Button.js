1012ba4d9e98c7613aa92f52d9e3c8d5c4dd7856
Yria Forján Oliveira <yriaforjan.oliveira@gmail.com>
2025-07-31 16:09:02 +0200

    fix: replaced <h4> with <p> inside button for better semantics

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,6 +3,6 @@ import "./Button.css";
 export const Button = (text, icon = null, isHome = false) => `
   <button class="myBtn ${icon ? "withIcon " : "withoutIcon"} ${isHome ? "fade-down" : ""}">
     ${icon ? `<img src="${icon}" alt="${text} icon"/>` : ""}
-    <h4>${text}</h4>
+    <p>${text}</p>
   </button>
 `;
